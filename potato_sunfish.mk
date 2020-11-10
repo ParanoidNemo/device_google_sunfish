c2fa0572f004a71deb54e327546d1ee1ac55e166
ParanoidNemo <paranoid.nemo@gmail.com>
2020-11-10 09:40:23 +0100

    potatoify

--- potato_sunfish.mk
+++ potato_sunfish.mk
@@ -1,5 +1,4 @@
-#
-# Copyright 2019 The Android Open Source Project
+# Copyright 2014 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,12 +11,22 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_sunfish.mk \
-    $(LOCAL_DIR)/potato_sunfish.mk \
+# Bootanimation
+#TARGET_BOOTANIMATION_SIZE := 1080p
 
-COMMON_LUNCH_CHOICES := \
-    aosp_sunfish-userdebug \
-    potato_sunfish-userdebug \
+# Inherit device parts
+$(call inherit-product, device/sony/suzu/aosp_sunfish.mk)
+
+#TARGET_KERNEL_CONFIG := aosp_loire_suzu_defconfig
+
+# Inherit Potato Stuff
+$(call inherit-product, vendor/potato/config/common_full_phone.mk)
+
+# Override Product Name
+
+PRODUCT_NAME := potato_sunfish
+PRODUCT_MODEL := Pixel 4a
+
+#Inherit ROM parts
+#$(call inherit-product, device/sony/common/potato.mk)
